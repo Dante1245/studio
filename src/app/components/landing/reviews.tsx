21f3f62df8603f6435fc56d058359ede3c40e62d
Clinton Saintkid <clintonsaintkid9971@gmail.com>
2025-08-09 02:12:02 +0000

    it still doesnt display picture fix the error

--- src/app/components/landing/reviews.tsx
+++ src/app/components/landing/reviews.tsx
@@ -1,7 +1,6 @@
 import { Card, CardContent } from '@/components/ui/card';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Star } from 'lucide-react';
-import Image from 'next/image';
 
 const reviews = [
   {
